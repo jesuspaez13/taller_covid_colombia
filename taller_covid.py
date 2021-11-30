0a68550a6672fedb316e9665bbb07d05ca66862b
Jesus Paez <jpaez13@cuc.edu.co>
2021-11-29 21:54:31 -0500

    Punto 3

--- taller_covid.py
+++ taller_covid.py
@@ -17,3 +17,7 @@ print(f'El numero de casos de contagios en el pais es de: {numero_de_casos}')
 # Punto 2
 municipios = len(data.groupby('Nombre municipio').size())
 print(f'EL numero de municipios afectado es: {municipios}')
+
+# Punto 3
+municipios_afectados = data.groupby('Nombre municipio').size().sort_values(ascending=False)
+print(f'\nMunicipios afectados: {municipios_afectados}')
