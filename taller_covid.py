feb25787e367cdf08a99e95c10a72b34748c8de4
Jesus Paez <jpaez13@cuc.edu.co>
2021-11-29 21:45:45 -0500

    Punto 1

--- taller_covid.py
+++ taller_covid.py
@@ -5,3 +5,12 @@ Created on Mon Nov 29 21:30:50 2021
 @author: USER
 """
 
+import pandas as pd
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+# Punto 1
+numero_de_casos = data.shape[0]
+print(f'El numero de casos de contagios en el pais es de: {numero_de_casos}')
+
